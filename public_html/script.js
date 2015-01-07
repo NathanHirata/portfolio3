1ba6efba33d036a784fd5fcbb67fbbc2e352a4b2
Nathan Hirata <nathanhirata@yahoo.com>
2015-01-07 15:02:51 -0800

    2nd commit

--- public_html/script.js
+++ public_html/script.js
@@ -1,3 +1,20 @@
 $("document").ready(function(){
     $(".yeah").css("background-color", "yellow");
 });
+
+$("document").ready(function(){
+    $(".red").css("background-color", "red");
+});
+
+$("document").ready(function(){
+    $(".yeah").css("color", "blue");
+});
+
+$("document").ready(function(){
+    $('.no').bind('click', alertButtonClick);
+});
+
+function alertButtonClick() {
+    alert("SIKE THATS THE WRONG NUMBER");
+}
+
