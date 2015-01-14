52c87dec01ea9e97f534407d7d23970f60968fd3
Nathan Hirata <nathanhirata@yahoo.com>
2015-01-14 12:28:04 -0800

    12

--- public_html/script.js
+++ public_html/script.js
@@ -1,25 +1,55 @@
 $("document").ready(function(){
-    $(".yeah").css("background-color", "yellow");
+    
+    $(".yeah").css("background-color", "darkblue");
+    
+    $(".projectColor").css("background-color", "lightblue");
+    
+    $(".k").css("color", "lightblue");
     
     $('replaceWText').bind('click', replaceWText);
     
     $('#slideAway').bind('click', slideTheImage);
+    
+    $('#showLogo').bind('click', showTheImage);
+    
+    $('#hideLogo').bind('click', hideTheImage);
+    
+    $('#showLogov2').bind('click', showTheImagev2);
+    
+    $('#hideLogov2').bind('click', hideTheImagev2);
+
 });
 
-function addApara() {
-    $()
+function slideTheImage() {
+    $('#logo').slideUp(2500);
+}
+
+function showTheImage() {
+    $('#logo').show('fold', {},2500);
+}
+
+function hideTheImage() {
+    $('#logo').hide('fold', {},2500);
+}
+
+function showTheImagev2() {
+    $('#logo').show('explode', {},2500);
 }
 
 function replaceWText() {
     $('#replaceWtext').text('Replaced!');
 }
 
+function hideTheImagev2() {
+    $('#logo').hide('explode', {},2500);
+}
+
 $("document").ready(function(){
-    $(".red").css("background-color", "red");
+    $(".red").css("background-color", "lime");
 });
 
 $("document").ready(function(){
-    $(".yeah").css("color", "blue");
+    $(".yeah").css("color", "white");
 });
 
 $("document").ready(function(){
